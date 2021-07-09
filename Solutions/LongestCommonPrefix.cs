69f8373761e53f14f3a0ea91e0697dba7ad7628e
Ryan Heitmann <ryan.heitmann@gmail.com>
2021-07-09 13:54:44 +1200

    Stopping mid problem - have to work

--- Solutions/LongestCommonPrefix.cs
+++ Solutions/LongestCommonPrefix.cs
@@ -7,28 +7,26 @@ namespace CSharp.Solutions
     {
         public string GetLongestCommonPrefix(string[] strings)
         {
-            char[] matchingChars = new char[strings[0].Length];
-            string wordToCheck = strings[0];
-            for (int i = 0; i < strings.Length; i++)
+            if (strings.Length < 1)
             {
-                if (strings[i].Length < 1)
-                {
-                    return new string(matchingChars).TrimEnd('\0');
-                }
-                char charToCheck = wordToCheck[i];
-                for (int j = 0; j < strings.Length; j++)
-                {
-                    if (wordToCheck.Length > strings[j].Length)
-                    {
-                        return new string(matchingChars).TrimEnd('\0');
-                    }
-                    else if (strings[j][i] != charToCheck)
-                    {
-                        return new string(matchingChars).TrimEnd('\0');
-                    }
-                }
-                matchingChars[i] = charToCheck;
+                return "";
             }
+            char[] matchingChars = new char[0];
+            string charactersToCheck = strings[0];
+            int letterIncrementer = 0;
+            int arrayIncrementer = 0;
+            
+            // index through array
+            foreach (string word in strings)
+            {
+                // index through each nth character of each word
+                // if index through entire array successful, add letter to matchingChars and continue to nextt characterToCheck
+                while (arrayIncrementer < strings.Length)
+                {
+                    
+                }
+            }
+
             return new string(matchingChars).TrimEnd('\0');
         }
     }
